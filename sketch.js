425d8513e58ac227bbb53d9daab4e6b6309ba1d6
analuisak <ana.kotelak@escola.pr.gov.br>
2025-05-30 11:54:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+xBolinha = 300;
+yBolinha = 200;
+diametro = 20;
+velocidadeBolinha = 6;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(33);
+  actorBolinha();
+}
+
+function actorBolinha()
+{
+  circle(xBolinha, yBolinha, diametro);
+  
+  xBolinha += velocidadeBolinha;
+  yBolinha += velocidadeBolinha;
 }
